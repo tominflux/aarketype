649d86e14f2515cf1fdbc81fb20e31a7a47765d6
Tom <tom.chesters96@gmail.com>
2020-07-29 13:34:43 +0100

    Added example app

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const { tshirtArchetype } = require("./archetypes/tshirt")
+const { createItem, validateItem } = require("../lib/item")
+
+
+const peaceTshirt = createItem(
+    tshirtArchetype,
+    {
+        name: "Peace T-Shirt",
+        priceGBP: 30.00,
+        description: "Peace logo, etc, blah, blah.",
+        ukPostGBP: 3.00,
+        wrldPostGBP: 8.00,
+    },
+    {
+        size: ["sm", "md", "lg", "xl"],
+        colourway: ["black", "white"]
+    }
+)
+
+validateItem(peaceTshirt, tshirtArchetype)
+
+console.log(peaceTshirt)
